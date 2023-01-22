1ac3389f284bf847fa4f5c38b7b2ff1f9830b48f
Exbyte112 <ismailaabubakar112@gmail.com>
2023-01-22 23:37:44 +0100

    okay

--- Ns.py
+++ Ns.py
@@ -1,32 +1,19 @@
 import json
-
-"""f = open("IgbotoNsibidi2.json") # an edited json file i made with accents removed
-words = json.load(f)
-words = dict(words)
-
-f2 = open("IgbotoNsibidi2.json") # the original json file u sent to me
-words2 = json.load(f2)
-words2 = dict(words2)
-
-#print(words)
-indeX = 1
-nS = words.keys() # the words list in Latin Igbo script
-Ig = words.values() # the words list in Nsibidi script
-
-nS2 = words2.keys() # the words list in Latin Igbo script
-Ig2 = words2.values() # the words list in Nsibidi script
-
-text = "Na mbu Chineke kere elu  igwe na uwa. Uwa we buru ihe tọb͕ọrọ n'efu na ihe tọb͕ọrọ nkití; ọchichiri di kwa n'elu ob͕u  miri: Mọ Chineke nērughari kwa n'elu miri. Chineke we si, Ka ihè di: ìhè we di. Chineke we hu ìhè ahu, na ọ di nma: Chineke we kpa ókè n'etiti ìhè ahu na ọchichiri ahu. Chineke we kpọ ìhè ahu Ehihie, ọchichiri ahu ka Ọ kpọkwara Abali. Anyasi we di, ututu we di, buru otù ubọchi.Chineke we si, Ka mbara di n'ab͕ata miri ahu, ka ọ nọ kwa nākpa ókè n'etiti miri na miri. Chineke we me mbara ahu, kpa ókè n'etiti miri ahu nke di n'okpuru mbara ahu na miri ahu nke di n'elu mbara ahu: ọ we di otú a. Chineke we kpọ mbara ahu Elu  igwe. Anyasi we di, ututu we di, buru ubọchi nke  abua.Chineke we si, Miri ahu nke di n'okpuru elu  igwe, ka achikọta ha n'otù ebe, ka ahú kwa ala  akọrọ: ọ we di otú a. Chineke we kpọ ala  akọrọ ahu Ala; nchikọta miri ahu ka Ọ kpọkwara Oké Osimiri: Chineke we hu na ọ di nma. Chineke we si, Ka ala puputa ahihia ndu, ihe  ọkukú nke nāmiputa nkpuru  ọghigha, osisi mkpuru nke nāmi mkpuru di iche iche, nke mkpuru  ọghi  gha  ya di nime ya, n'elu ala: o we di otú a. Ala we weputa ahihia ndu, ihe  ọkukú nke nāmiputa nkpuru  ọghigha di iche iche, ya na osisi nke nāmi mkpuru di iche iche, nke nkpuru  ọghigha  ya di nime ya: Chineke we hu na ọ di nma. Anyasi we di, ututu we di, buru ubọchi nke  atọ."
-""" # all of this is useless for now but i haven't deleted coz it might be useful later on
+import random
 # ==================================
 # PROGRAM STARTS HERE
 
-def userPrompt(result = "bụ"):
-    return result
-latin_sent = userPrompt() # input any value into the bracket to translate that value or leave it blank to translate the default value ("bụ")
+sample = "ọkpà"
+
 nsibidi = open("IgbotoNsibidi.json" , encoding="utf8")
 nsibidi = json.load(nsibidi)
 nsibidi = dict(nsibidi)
+
+randomiser = random.choice(list(nsibidi.keys()))
+
+def userPrompt(result = sample):
+    return result
+latin_sent = userPrompt() # input any value into the bracket to translate that value or leave it blank to translate a random word
 
 oov = []  # 'oov' stands for 'out of vocabulary', it's a common problem in nlp. This in this list you'll store all your unaccounted for words that don't have a corresponding Nsibidi in your dictionary
 
@@ -42,15 +29,38 @@ nsibidi_sent = nsibidi_sent[0]
 ans = nsibidi_sent
 
 print(oov)
+print(f"===============\nLATIN: {userPrompt()}\n===============")
 
+"""ans = latin_sent
+answer = []
+for i in ans.split():
+    print(str(type(nsibidi[i]))+ " ".join(nsibidi[i]) +"ok")
+    if type(nsibidi[i]) == list:
+        print("Choose an answer")
+        for j in range(len(nsibidi[i])):
+            print(j, nsibidi[i][j])
+        ans = int(input("Enter the number of the answer you want: "))
+        answer.append(nsibidi[i][ans])
+    elif type(nsibidi[i]) == str:
+        answer.append(nsibidi_sent)
+    else:
+        print("error")
+print(' '.join(answer))"""
 
-def randomAnswer(ans = nsibidi_sent):
-    if type(nsibidi_sent) == list:
-        import random
-        ans = random.choice(nsibidi_sent)
-        return (' '.join(ans))
-    elif type(nsibidi_sent) == str:
-        return (' '.join(ans))
+def randomAnswers(ans = latin_sent, ans2 = nsibidi_sent):
+    answer = []
+    tester = 0
+    for i in ans.split():
+        if type(nsibidi[i]) == list:
+            answer.append(random.choice(nsibidi[i]))
+            tester = 0
+        elif type(nsibidi[i]) == str:
+            answer.append(nsibidi[i])
+            tester = 1
+        else:
+            answer.append("error")
+            tester = 2
+    return(' '.join(answer) + str(tester))
 
 def choiceAnswer(ans = nsibidi_sent):
     #instead of picking a random answer this will ask the user what answer they want
@@ -60,10 +70,38 @@ def choiceAnswer(ans = nsibidi_sent):
             print(i, nsibidi_sent[i])
         ans = int(input("Enter the number of the answer you want: "))
         return (' '.join(nsibidi_sent[ans]))
+    else:
+        return(' '.join(nsibidi_sent))
 
 answerGetter = input("Do you want a random answer or a choice based answer? (r/c): ")
 answerGetter = answerGetter.lower() # this converts whichever case to lower
 if answerGetter == "r":
-    print(randomAnswer() + "\n=========================\nthis is a random generated result")
+    print("Nsibidi: " + randomAnswers() + "\n=========================\nthis is a random generated result")
 elif answerGetter == "c":
-    print(choiceAnswer() + "\n=========================\nthis is a choice based result")
+    print("Nsibidi: " + str(choiceAnswer()) + "\n=========================\nthis is a choice based result")
+
+#============================================
+# No longer useful code
+"""def randomAnswer(ans = nsibidi_sent):
+    if type(nsibidi_sent) == list:
+        import random
+        ans = random.choice(nsibidi_sent)
+        return (' '.join(ans))
+    elif type(nsibidi_sent) == str:
+        return (' '.join(ans))"""
+
+"""def choiceAnswers(ans = latin_sent):
+    answer = []
+    for i in ans.split():
+        print(str(type(nsibidi[i]))+ " ".join(nsibidi[i]) +"ok")
+        if type(nsibidi[i]) == list:
+            print("Choose an answer")
+            for j in range(len(nsibidi[i])):
+                print(j, nsibidi[i][j])
+            ans = int(input("Enter the number of the answer you want: "))
+            answer.append(nsibidi[i][ans])
+        elif type(nsibidi[i]) == str:
+            answer.append(nsibidi_sent)
+        else:
+            print("error")
+    return(' '.join(answer))"""
