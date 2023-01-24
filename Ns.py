6eeeda2afdc6e434d5f65977f3ed4a6a7df6adb2
Exbyte112 <ismailaabubakar112@gmail.com>
2023-01-24 09:02:02 +0100

    fixed the choice selector

--- Ns.py
+++ Ns.py
@@ -3,7 +3,7 @@ import random
 # ==================================
 # PROGRAM STARTS HERE
 
-sample = "ọkpà"
+sample = "ikpe i kpe ekpere e kpe re"
 
 nsibidi = open("IgbotoNsibidi.json" , encoding="utf8")
 nsibidi = json.load(nsibidi)
@@ -24,28 +24,39 @@ for i in latin_sent.split():
         oov.append(i)
     else:
         nsibidi_sent.append(nsibidi[i])
-
-nsibidi_sent = nsibidi_sent[0]
+if nsibidi_sent == []:
+    print("No words found")
+    runner = False
+else:
+    nsibidi_sent = nsibidi_sent[0]
+    runner = True
 ans = nsibidi_sent
 
-print(oov)
+print(f"These words were not found: {oov}")
 print(f"===============\nLATIN: {userPrompt()}\n===============")
 
-"""ans = latin_sent
-answer = []
-for i in ans.split():
-    print(str(type(nsibidi[i]))+ " ".join(nsibidi[i]) +"ok")
-    if type(nsibidi[i]) == list:
-        print("Choose an answer")
-        for j in range(len(nsibidi[i])):
-            print(j, nsibidi[i][j])
-        ans = int(input("Enter the number of the answer you want: "))
-        answer.append(nsibidi[i][ans])
-    elif type(nsibidi[i]) == str:
-        answer.append(nsibidi_sent)
-    else:
-        print("error")
-print(' '.join(answer))"""
+def choiceAnswers(ans = latin_sent):
+    answer = []
+    count = 1
+    for i in ans.split():
+        previewText = " ".join(answer)
+        print(f"Completed: {previewText} ________ ")
+        if type(nsibidi[i]) == list:
+            print("Choose an answer")
+            for j in range(len(nsibidi[i])):
+                print(j, nsibidi[i][j])
+            ans = int(input("Enter the number of the answer you want: "))
+            answer.append(nsibidi[i][ans])
+            count += 1
+        elif type(nsibidi[i]) == str:
+            answer.append(nsibidi[i])
+            print("String added")
+            count += 1
+        else:
+            print("error")
+            count += 1
+    answer = ' '.join(answer)
+    return answer
 
 def randomAnswers(ans = latin_sent, ans2 = nsibidi_sent):
     answer = []
@@ -62,23 +73,17 @@ def randomAnswers(ans = latin_sent, ans2 = nsibidi_sent):
             tester = 2
     return(' '.join(answer) + str(tester))
 
-def choiceAnswer(ans = nsibidi_sent):
-    #instead of picking a random answer this will ask the user what answer they want
-    if type(nsibidi_sent) == list:
-        print("Choose an answer")
-        for i in range(len(nsibidi_sent)):
-            print(i, nsibidi_sent[i])
-        ans = int(input("Enter the number of the answer you want: "))
-        return (' '.join(nsibidi_sent[ans]))
+if runner == True:
+    answerGetter = input("Do you want a random answer or a choice based answer? (r/c): ")
+    answerGetter = answerGetter.lower() # this converts whichever case to lower
+    if answerGetter == "r":
+        print("Nsibidi: " + randomAnswers() + "\n=========================\nthis is a random generated result")
+    elif answerGetter == "c":
+        print("Nsibidi: " + str(choiceAnswers()) + "\n=========================\nthis is a choice based result")
     else:
-        return(' '.join(nsibidi_sent))
-
-answerGetter = input("Do you want a random answer or a choice based answer? (r/c): ")
-answerGetter = answerGetter.lower() # this converts whichever case to lower
-if answerGetter == "r":
-    print("Nsibidi: " + randomAnswers() + "\n=========================\nthis is a random generated result")
-elif answerGetter == "c":
-    print("Nsibidi: " + str(choiceAnswer()) + "\n=========================\nthis is a choice based result")
+        print("Invalid input")
+else:
+    print("No words found")
 
 #============================================
 # No longer useful code
@@ -90,18 +95,15 @@ elif answerGetter == "c":
     elif type(nsibidi_sent) == str:
         return (' '.join(ans))"""
 
-"""def choiceAnswers(ans = latin_sent):
-    answer = []
-    for i in ans.split():
-        print(str(type(nsibidi[i]))+ " ".join(nsibidi[i]) +"ok")
-        if type(nsibidi[i]) == list:
-            print("Choose an answer")
-            for j in range(len(nsibidi[i])):
-                print(j, nsibidi[i][j])
-            ans = int(input("Enter the number of the answer you want: "))
-            answer.append(nsibidi[i][ans])
-        elif type(nsibidi[i]) == str:
-            answer.append(nsibidi_sent)
-        else:
-            print("error")
-    return(' '.join(answer))"""
+"""def choiceAnswer(ans = nsibidi_sent):
+    #instead of picking a random answer this will ask the user what answer they want
+    if type(nsibidi_sent) == list:
+        print("Choose an answer")
+        for i in range(len(nsibidi_sent)):
+            print(i, nsibidi_sent[i])
+        ans = int(input("Enter the number of the answer you want: "))
+        return (' '.join(nsibidi_sent[ans]))
+    else:
+        return(' '.join(nsibidi_sent))"""
+
+
